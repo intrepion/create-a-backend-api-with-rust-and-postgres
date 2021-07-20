34ff88c27098960216ef2ac7e84cf0455efdbd4f
Oliver Forral <intrepion@gmail.com>
2021-07-19 20:44:29 -0700

    Creating routes for our API

--- emply/src/main.rs
+++ emply/src/main.rs
@@ -1,14 +1,11 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-async fn welcome(request: HttpRequest) -> impl Responder {
-    let name = request.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+mod employees;
+
+use actix_web::{App, HttpServer};
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(welcome))
-            .route("/{name}", web::get().to(welcome))
+        App::new().configure(employees::init_routes)
     })
     .bind("127.0.0.1:8000")?
     .run()
